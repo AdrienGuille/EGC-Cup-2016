64d9e3c5f6051ed4bb8380482f72a4826f5c5c98
Adrien Guille <adrien.guille@gmail.com>
2015-07-08 21:45:26 +0200

    Add basic graph analytics

--- first_experiments.py
+++ first_experiments.py
@@ -4,7 +4,9 @@ __email__ = "adrien.guille@univ-lyon2.fr"
 
 from gensim import corpora, models
 from nltk.corpus import stopwords
+from nltk import wordpunct_tokenize
 from collections import defaultdict
+import networkx as nx
 
 
 def load_corpus(file_path):
@@ -28,9 +30,39 @@ def abstract_list(corpus):
         abstracts.append(article.get('abstract'))
     return abstracts
 
+def author_set(corpus):
+    authors = set()
+    for article in corpus:
+        for author in article.get('authors'):
+            authors.add(author)
+    return authors
+
+def create_collaboration_graph(corpus):
+    graph = nx.Graph()
+    graph.add_nodes_from(author_set(corpus))
+    for article in corpus:
+        authors = article.get('authors')
+        for i in range(0, len(authors)):
+            for j in range(i+1, len(authors)):
+                graph.add_edge(authors[i], authors[j])
+    print graph.number_of_edges(), 'distinct collaborations'
+    return graph
+
+def export_collaboration_graph(graph):
+    graph.write_gml(graph, 'collaboration.gml')
+
+def analyze_collaboration_graph(graph):
+    if nx.is_connected(graph):
+        print 'diameter', nx.diameter(graph)
+    else:
+        print 'disconnected graph'
+    k_core = nx.core_number(graph)
+    print 'k-core decomposition: ', sorted(k_core.items(), key=lambda x: x[1], reverse=True)
+
+
 def train_lda(abstracts, num_topics):
     stopword_list = stopwords.words('french')
-    formatted_abstracts = [[word for word in abstract.lower().split() if word not in stopword_list] for abstract in abstracts]
+    formatted_abstracts = [[word for word in wordpunct_tokenize(abstract.lower()) if word not in stopword_list] for abstract in abstracts]
     frequency = defaultdict(int)
     for formatted_abstract in formatted_abstracts:
         for word in formatted_abstract:
@@ -52,24 +84,10 @@ def print_topics(topics):
         print('topic',count,': ',' '.join(word_list))
         count += 1
 
-def create_collaboration_graph(corpus):
-    collaborations = set()
-    for article in corpus:
-        authors = article.get('authors')
-        year = article.get('year')
-        for i in range(0, len(authors)):
-            for j in range(i+1, len(authors)):
-                collaboration = authors[i]+'\t'+authors[j]
-                collaborations.add(collaboration)
-    print len(collaborations), 'collaborations'
-    return collaborations
-
-def export_collaboration_graph(file_path, edge_set):
-    output_file = open(file_path, 'w')
-    output_file.write("\n".join(edge_set))
-
 article_corpus = load_corpus('RNTI_articles_export_utf-8.txt')
 collaboration_graph = create_collaboration_graph(article_corpus)
-export_collaboration_graph('all_collaborations.csv', collaboration_graph)
-latent_topics = train_lda(abstract_list(article_corpus), 20)
-print_topics(latent_topics)
+# export_collaboration_graph(collaboration_graph)
+analyze_collaboration_graph(collaboration_graph)
+print 'degree(Adrien Guille): ', collaboration_graph.degree('Adrien Guille')
+# latent_topics = train_lda(abstract_list(article_corpus), 20)
+# print_topics(latent_topics)
