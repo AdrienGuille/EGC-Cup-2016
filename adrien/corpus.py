47b786ff3aa9df33a21d74764083e9884cd5187b
Adrien Guille <adrien.guille@gmail.com>
2015-07-09 16:08:55 +0200

    Add serialization

--- adrien/corpus.py
+++ adrien/corpus.py
@@ -5,9 +5,10 @@ __email__ = "adrien.guille@univ-lyon2.fr"
 from langdetect import detect
 import networkx as nx
 import codecs
+import pickle
 
 
-def load_corpus(file_path, year_a=2004, year_b=2014, enforce_language=None):
+def load(file_path, year_a=2004, year_b=2014, enforce_language=None):
     input_file = codecs.open(file_path, 'r', encoding='latin-1')
     count = 0
     article_list = []
@@ -23,6 +24,12 @@ def load_corpus(file_path, year_a=2004, year_b=2014, enforce_language=None):
                 article_list.append({'title': article[3], 'year': article[2], 'authors': authors, 'abstract': article[4], 'language': language})
     print count, 'articles (', enforce_language, ')'
     return article_list
+
+def serialize(corpus, file_path):
+    pickle.dump(corpus, open(file_path, 'wb'))
+
+def deserialize(file_path):
+    return pickle.load(open(file_path, 'rb'))
 
 def abstract_list(corpus):
     abstracts = []
--- adrien/main.py
+++ adrien/main.py
@@ -6,11 +6,23 @@ import corpus
 import graph_mining
 import text_mining
 
-french_article_corpus = corpus.load_corpus('input/RNTI_articles_export.txt', 2004, 2015, 'fr')
-english_article_corpus = corpus.load_corpus('input/RNTI_articles_export.txt', 2004, 2015, 'en')
-complete_article_corpus = corpus.load_corpus('input/RNTI_articles_export.txt', 2004, 2015)
-collaboration_graph = corpus.collaboration_graph(complete_article_corpus)
-graph_mining.analyze_collaboration_graph(collaboration_graph)
-print 'degree(Adrien Guille): ', collaboration_graph.degree('Adrien Guille')
+## Load articles from the original .txt file and serialize 3 corpora: (i) French articles, (ii) English articles, (iii) all articles
+# french_article_corpus = corpus.load('input/RNTI_articles_export.txt', 2004, 2015, 'fr')
+# corpus.serialize(french_article_corpus, 'output/french_articles.pickle')
+# english_article_corpus = corpus.load('input/RNTI_articles_export.txt', 2004, 2015, 'en')
+# corpus.serialize(english_article_corpus, 'output/english_articles.pickle')
+# complete_article_corpus = corpus.load('input/RNTI_articles_export.txt', 2004, 2015)
+# corpus.serialize(complete_article_corpus, 'output/all_articles.pickle')
+
+## Deserialize corpora saved on disk
+french_article_corpus = corpus.deserialize('output/french_articles.pickle')
+english_article_corpus = corpus.deserialize('output/english_articles.pickle')
+complete_article_corpus = corpus.deserialize('output/all_articles.pickle')
+
+# Get French abstracts and train LDA for k = 20
 latent_topics = text_mining.train_lda(corpus.abstract_list(french_article_corpus), 20)
 text_mining.print_topics(latent_topics)
+
+# Extract the collaboration graph for all the articles and compute the k-core decomposition
+collaboration_graph = corpus.collaboration_graph(complete_article_corpus)
+graph_mining.analyze_collaboration_graph(collaboration_graph)
